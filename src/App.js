900b69fb54a99bc9b38b11b3e98076d95077b500
Joseph Kalb <kalb.joseph@gmail.com>
2019-06-08 15:53:14 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    endpoint: "localhost:8001",
+    question:""
+  }
+  
+  render() {
+    const socket = socketIOClient(this.state.endpoint)
+
+    socket.on('news', (data) => {
+      console.log(data)
+    })
+
+    socket.on('question', (info) => {
+      console.log(info)
+      this.setState({question: info.question.question})
+    })
+
+    return(
+      <div>
+        <p>{this.state.question}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
